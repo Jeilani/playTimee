25a32692ad1b52b38e64623e7645b34f5252a046
Jeilani Mohamed <Jeilani@Jeilanis-MacBook-Pro.local>
2020-09-10 10:55:23 -0600

    Updates

---
+++ precache-manifest.7de7f62463259bf66eff40b30a122125.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4a927012e2b0c625ec49bcd2f566e6",
+    "url": "/playTimee/index.html"
+  },
+  {
+    "revision": "044cac28294e9fa5f8a2",
+    "url": "/playTimee/static/css/2.e055089f.chunk.css"
+  },
+  {
+    "revision": "9710f1fc088a69f528d8",
+    "url": "/playTimee/static/css/main.25c3da5b.chunk.css"
+  },
+  {
+    "revision": "044cac28294e9fa5f8a2",
+    "url": "/playTimee/static/js/2.cbc3d2b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/playTimee/static/js/2.cbc3d2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9710f1fc088a69f528d8",
+    "url": "/playTimee/static/js/main.6e9413fb.chunk.js"
+  },
+  {
+    "revision": "55969b5410b0b4d5576f",
+    "url": "/playTimee/static/js/runtime-main.23196ec2.js"
+  },
+  {
+    "revision": "4f097d08cbbf1d88be363869364086e4",
+    "url": "/playTimee/static/media/ParkBasketball.4f097d08.jpeg"
+  },
+  {
+    "revision": "7f3476c609bb6603ef046733a6302acf",
+    "url": "/playTimee/static/media/baseball.7f3476c6.svg"
+  },
+  {
+    "revision": "b08ce6f0ac8e0f4fb6e3e4cd8a33489e",
+    "url": "/playTimee/static/media/baseballcover.b08ce6f0.jpeg"
+  },
+  {
+    "revision": "8ee1217756633dee2cc5980fa3ff0207",
+    "url": "/playTimee/static/media/basketball.8ee12177.svg"
+  },
+  {
+    "revision": "e7d5f15de4a3d960c9815591dc59493c",
+    "url": "/playTimee/static/media/basketballcover2.e7d5f15d.jpeg"
+  },
+  {
+    "revision": "695bda4531b634d1ed93d80532a3cf19",
+    "url": "/playTimee/static/media/dummy-headshot.695bda45.jpg"
+  },
+  {
+    "revision": "805a60dfe572d38c6ec03623911d76ec",
+    "url": "/playTimee/static/media/football.805a60df.svg"
+  },
+  {
+    "revision": "2a9a847b677875093ec5f7794622a288",
+    "url": "/playTimee/static/media/footballcover.2a9a847b.jpeg"
+  },
+  {
+    "revision": "87d9e26906c3bdd4d02df38474c5cebb",
+    "url": "/playTimee/static/media/hikingcover.87d9e269.jpeg"
+  },
+  {
+    "revision": "e9e66d39fda37586e0e628cccc5efa32",
+    "url": "/playTimee/static/media/soccer.e9e66d39.svg"
+  },
+  {
+    "revision": "1769df76c88f802f4290a4528d2ab47d",
+    "url": "/playTimee/static/media/soccercover.1769df76.jpeg"
+  },
+  {
+    "revision": "e71c8176d44469dcf2ac5648fd8677f0",
+    "url": "/playTimee/static/media/tennis.e71c8176.svg"
+  }
+]);
