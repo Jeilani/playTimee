9b479873844546a4219cefa49d4b77058ae6a7c5
Jeilani Mohamed <Jeilani@Jeilanis-MacBook-Pro.local>
2020-09-10 09:57:23 -0600

    changed from build folder to public

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'client', 'build');
+const publicPath = path.join(__dirname, '..', 'client', 'public');
 const port = process.env.PORT || 3000;
 
 
